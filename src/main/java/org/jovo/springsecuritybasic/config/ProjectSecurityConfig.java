d0f52009e5f5ac8f763680d07e5de3185eda7744
jb <usfxcsgowaheqpvzlp@kvhrr.com>
2024-01-25 11:36:08 +0100

    Implement InMemoryUserDetailsManager in security configuration
    The update involves modifying the ProjectSecurityConfig.java file in order to set up InMemoryUserDetailsManager for user authentication. This involved adding predefined user details for an 'admin' and a 'user', along with their respective authorities. It also includes the removal of the previously hardcoded user credentials in the application.yml file, making the system more secure and robust.

--- src/main/java/org/jovo/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/org/jovo/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -7,14 +7,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig
 {
+  /**
+   * Creates the default security filter chain for the HTTP security configuration.
+   *
+   * @param http the HttpSecurity object to configure
+   * @return the configured SecurityFilterChain
+   * @throws Exception if an error occurs during configuration
+   */
   @Bean
   @Order(SecurityProperties.BASIC_AUTH_ORDER)
-  SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests(requests -> requests
             .requestMatchers("contact", "notices").permitAll()
@@ -23,5 +33,29 @@ public class ProjectSecurityConfig
     http.formLogin(withDefaults());
     http.httpBasic(withDefaults());
     return http.build();
+  }
+
+  /**
+   * Returns an instance of InMemoryUserDetailsManager configured with predefined user details.
+   *
+   * @return an instance of InMemoryUserDetailsManager
+   */
+  @Bean
+  @SuppressWarnings("java:S1874")
+  public InMemoryUserDetailsManager userDetailsService()
+  {
+    UserDetails admin = User.withDefaultPasswordEncoder()
+        .username("admin")
+        .password("1234")
+        .authorities("admin")
+        .build();
+
+    UserDetails user = User.withDefaultPasswordEncoder()
+        .username("user")
+        .password("1234")
+        .authorities("read")
+        .build();
+
+    return new InMemoryUserDetailsManager(admin, user);
   }
 }
